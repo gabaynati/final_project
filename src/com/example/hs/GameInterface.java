4dddb1cb54c790b32a7cab8c7762014fd02074cd
gili123 <gili1054@gmail.com>
2015-12-04 10:53:59 +0200

    one shoot audio

--- src/com/example/hs/GameInterface.java
+++ src/com/example/hs/GameInterface.java
@@ -1,20 +1,25 @@
 package com.example.hs;
 
 import java.io.IOException;
+
 import android.app.Activity;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.FrameLayout;
 
-public class GameInterface extends Activity {
+public class GameInterface extends Activity implements OnTouchListener {
 	
 	final static String TAG = "PAAR";
 	private SurfaceHolder previewHolder;
 	private Camera camera = null;
 	private SurfaceView cameraPreview;
+	private M4a1 m4a1;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +30,19 @@ public class GameInterface extends Activity {
         previewHolder = cameraPreview.getHolder();
         previewHolder.addCallback(surfaceCallback);
         previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        
-        
+          
         FrameLayout gameLayout = (FrameLayout)findViewById(R.id.game_layout);
-        M4a1 m = new M4a1(getApplicationContext());
-        gameLayout.addView(m);
-        
+        m4a1 = new M4a1(getApplicationContext());
+        gameLayout.addView(m4a1);
+   
+        m4a1.setOnTouchListener(this);
 	}
 
 	
 
-	 SurfaceHolder.Callback surfaceCallback=new SurfaceHolder.Callback() {
-	    	public void surfaceCreated(SurfaceHolder holder) {
+	 SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+
+			public void surfaceCreated(SurfaceHolder holder) {
 	    		camera = Camera.open();
 	    		try {
 	    			camera.setPreviewDisplay(previewHolder);
@@ -45,19 +51,29 @@ public class GameInterface extends Activity {
 	    		}
 	    	}
 	    	
-	    	public void surfaceChanged(SurfaceHolder holder, int format, int width,	int height) {
-	    		Camera.Parameters params = camera.getParameters();
+
+			public void surfaceChanged(SurfaceHolder holder, int format, int width,	int height) {
+
+				Camera.Parameters params = camera.getParameters();
 	    		params.setPreviewSize(width, height);
 	    		camera.setParameters(params);
 	    		camera.startPreview();
 		}
 	    	
-	    	public void surfaceDestroyed(SurfaceHolder holder) {
+
+			public void surfaceDestroyed(SurfaceHolder holder) {
 	    		camera.stopPreview();
 	    		camera = null;
 	    	}
 	    	
 	    };
+
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		m4a1.shoot();
+		return false;
+	}
   
 
 
--- src/com/example/hs/m4a1.java
+++ src/com/example/hs/m4a1.java
@@ -2,25 +2,46 @@ package com.example.hs;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 
-public class m4a1 extends View {
+public class M4a1 extends View implements OnTouchListener {
 
-	public m4a1(Context context) {
+	private Drawable image;
+	private MediaPlayer player;
+	
+	public M4a1(Context context) {
 		super(context);
+		
+		image = getResources().getDrawable(R.drawable.m4a1);
+		player = MediaPlayer.create(context, R.raw.m4a1_single);
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		/*
-		canvas.drawColor(Color.BLACK);
-		Paint p = new Paint();
-		p.setColor(Color.RED);
-		canvas.drawCircle(canvas.getWidth()/4, canvas.getHeight()/4, canvas.getWidth()/6, p);
-		*/
+		
+		int left, top, right, bottom;
+		
+		left = canvas.getWidth()/2 - 150;
+		top = canvas.getHeight() - 168;
+		right = canvas.getWidth()/2 + 150;
+		bottom = canvas.getHeight();
+		
+		image.setBounds(left, top, right, bottom);
+		image.draw(canvas);
 	}
 
+	public void shoot(){
+		player.start();
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		player.start();
+		return false;
+	}
 }
